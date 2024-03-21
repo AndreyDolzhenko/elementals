e88424808bc3f7d827f1845ae9642da52edfac4c
AndreyDolzhenko <adol2000@rambler.ru>
2024-03-21 18:14:54 +0300

    21032024

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,32 @@ import './App.css'
 function App() {
   const [text, setText] = useState('');  
   const [listItems, setListItems] = useState<string[]>([]);
-  
+
+  let getCo;
+  function getContent(content: any) {
+    return (
+      <li>{content}</li>      
+    );
+  }
+ 
 console.log(text);
   return (    
-    <>
-      <input type="text" value={text} onChange={(e: any) => setText(e.target.value)}/>
-      <button onClick={() => setListItems([...listItems, text])}>Отправить</button>
-      <ol>
-        {listItems.map((el) => (
+    <>    
+      
+      <input type="text" value={text} onChange={(e: any) => (
+        setText(e.target.value)
+        )}/>
+      <button onClick={() => (
+        // setListItems([...listItems, text])
+        getCo = getContent(text)
+        // console.log(getContent(text))
+        )}>Отправить</button>
+        
+      <ol>   
+        {getCo}     
+        {/* {listItems.map((el) => (
           <li>{el}</li>
-        ))}
+        ))} */}
       </ol>
     </>
   );
