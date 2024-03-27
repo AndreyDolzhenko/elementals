d6127e2518a7e701b838b6cdeec5e862a73c3df5
AndreyDolzhenko <adol2000@rambler.ru>
2024-03-27 17:57:17 +0300

    state

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,84 @@
-import { useState } from 'react';
-import { useRef } from 'react';
-import './App.css'
+import { useState } from "react";
+import { useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [text, setText] = useState('');  
-  const [listItems, setListItems] = useState<string[]>([]);
-  const divRef = useRef(null);
-
-  function Logo() {
+  // const [text, setText] = useState('');
+  // const [listItems, setListItems] = useState<string[]>([]);
+  // const divRef = useRef(null);
+  const logo = 'LEMENTALS'.split('');
+  const [logoLetter, setLogoLetter] = useState('');
+  
+   function Logo() {    
     return (
-      <div id='logo'>
-        <div id='logoText'>LEMENTALS</div>
+      <div style={{ width: 200 }}>
+        <div id="logo">
+          <div id="logoText" style={{color: 'green'}}>{logoLetter}</div>          
+        </div>
       </div>
     );
   }
 
   function Header() {
-
     return (
-      <>
-      {Logo()} 
-        <div>
+      <div id="header">
+        {Logo()}
+        <div id="headerMenu">
           <span>Episodes</span>
           <span>About</span>
           <span>More</span>
         </div>
-      </>
+        <div id="headerButton">
+          <button>Subscribe</button>
+          <button>Registration</button>
+        </div>
+      </div>
     );
   }
-  
-  function completionOfInput(content: any) {   
-    if (content.key == 'Enter' || content.target.value == 'button') {           
-      setListItems([...listItems, text]);
-      // divRef.current.innerHTML += `<br>${text}`;           
-      setText('');   
+
+  // function completionOfInput(content: any) {
+  //   if (content.key == 'Enter' || content.target.value == 'button') {
+  //     setListItems([...listItems, text]);
+  //     // divRef.current.innerHTML += `<br>${text}`;
+  //     setText('');
+  //   }
+  // }
+
+  // function deleteTask(index: number) {
+  //   setListItems((prev) => (
+  //     prev.filter((el, i) => index !== i)
+  //   ));
+  // }
+
+  return (
+    <>
+    {
+      
+        
+         
+        
+          
+        logo.forEach((el, i) => (
+          
+          setInterval(() => {
+         
+          setLogoLetter(logo[i]);
+          console.log([...logoLetter, logo[i]]);
+        }, 2000)
+          ))   
+        // setLogoLetter(logo);
+        // console.log([...logoLetter, logo]);
+
+     
     }
-  }
- 
-  function deleteTask(index: number) {
-    setListItems((prev) => (
-      prev.filter((el, i) => index !== i)
-    ));
-  }
-  
-  return (    
-    <>     
       <div>{Header()}</div>
+
+
+
+
+      {/* <div>{logoLetter}</div> */}
+
+      {/* 
       <input type="text" value={text}
       onKeyUp={(e: any) => (completionOfInput(e))}
       onChange={(e: any) => (setText(e.target.value))}
@@ -62,9 +95,10 @@ function App() {
           </li>
         ))}
       </ol>
-      {/* <div ref = {divRef} /> */}
+      <div ref = {divRef} />
+ */}
     </>
   );
 }
 
-export default App
+export default App;
