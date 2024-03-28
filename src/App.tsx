d58a6c0728edf2a7742fb4d253da0a8eb4157362
AndreyDolzhenko <adol2000@rambler.ru>
2024-03-28 16:46:34 +0300

    28032024

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,96 @@
 import { useState } from "react";
 import { useRef } from "react";
+import PersonGirl from "./svg/girl1.svg";
+import PersonBoy from "./svg/boy1.svg";
+import VK from "./svg/vk_1.svg";
+import Facebook from "./svg/facebook_1.svg";
+import Odnoklassniki from "./svg/odnoklassniki_1.svg";
+import Google from "./svg/google.svg";
 import "./App.css";
 
-function App() {
-  // const [text, setText] = useState('');
-  // const [listItems, setListItems] = useState<string[]>([]);
-  // const divRef = useRef(null);
+function App() {  
   
-  const [logoLetter, setLogoLetter] = useState("LEMENTALS");
-  const [count, setCount] = useState(0)
+  const avaGirl = <img src={PersonGirl} alt="My SVG" />;
+  const avaBoy = <img src={PersonBoy} alt="My SVG" />;
+  const [person, setPerson] = useState(avaBoy);
+  const [auto, setAuto] = useState("");
 
-  function Logo() {
-    return (
-      <div style={{ width: 200 }}>
+  
+function authorization() {
+  return (
+    <div className="authorizationBaze">
+      <div className="authorizationForm">        
+      <div style={
+      {
+        textAlign: "end",        
+        fontSize: "20px",
+        color: "green",
+        cursor: "pointer",
+        margin: "-50px -30px 0 0"
+      }
+    }
+    onClick={() => setAuto("")}
+    >X</div>
+    <div style={
+      {
+        display: "flex",
+        justifyContent: "space-between",
+        margin: "30px"
+      }
+    }>
+    <div>Авторизация</div>
+    <div style={{textAlign: "end"}}>
+      <img style={{width: "5%", cursor: "pointer"}} src={VK} alt="My SVG" />
+      <img style={{width: "5%", cursor: "pointer"}} src={Facebook} alt="My SVG" />
+      <img style={{width: "5%", cursor: "pointer"}} src={Odnoklassniki} alt="My SVG" />
+      <img style={{width: "5%", cursor: "pointer"}} src={Google} alt="My SVG" />
+      
+    </div>
+    </div>
+    <input type="text" placeholder="Логин" />
+    <input type="text" placeholder="Пароль" />
+    <button className="authorizationButton">Войти</button>
+    <div></div>
+    </div>
+    </div>
+  )
+}
+
+  function header() {
+    return (      
+      <header>
+        <div style={{
+          display: "flex", 
+          alignItems: "center"
+        }}
+        >
+        <div style={{ width: 250 }}>
         <div id="logo">
-          <div id="logoText" style={{ color: "green" }}>
-            {logoLetter}
-          </div>
+          <div id="logoText" style={{ 
+            color: "green",
+            letterSpacing: ".1rem"
+            }}>LEMENTALS</div>
         </div>
       </div>
-    );
-  }
-
-  function Header() {
-    return (
-      <div id="header">
-        <Logo />
         <div id="headerMenu">
           <span>Episodes</span>
           <span>About</span>
           <span>More</span>
         </div>
-        <div id="headerButton">
-          <button>Subscribe</button>
-          <button>Registration</button>
         </div>
-      </div>
+        <div id="headerButton">
+          <button>SUBSCRIBE</button>
+          <div title="войти" style={
+            {width: 70, cursor: "pointer"}
+          } 
+          onMouseOver={() => setPerson(avaGirl)}
+          onMouseOut={() => setPerson(avaBoy)}
+          onClick={() => setAuto(authorization())}
+          >
+            {person}
+          </div>
+        </div>        
+      </header>
     );
   }
 
@@ -43,15 +98,8 @@ function App() {
 
   return (
     <>
-      
-      <div>{Header()}</div>
-      <div>
-        <input type="text"  onClick={(e) => console.log(e.target.value)}/>
-        <button onClick={() => setCount(count - 1)}>-</button>
-        <span>{count}</span>
-        <button onClick={() => setCount(count + 1)}>+</button>
-      </div>
-
+      <>{auto}</>
+      <div>{header()}</div> 
       
     </>
   );
