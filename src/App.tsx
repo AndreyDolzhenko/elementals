b515da75ccfe23f317ed537e73b2baf18c7c5d66
AndreyDolzhenko <adol2000@rambler.ru>
2024-04-02 18:00:44 +0300

    02042024

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 // import authorization from "./authorization";
-import PersonGirl from "./svg/girl1.svg?react";
-import PersonBoy from "./svg/boy1.svg?react";
 import "./App.css";
 
 import VK from "./svg/vk_1.svg?react";
@@ -9,112 +7,73 @@ import Facebook from "./svg/facebook_1.svg?react";
 import Odnoklassniki from "./svg/odnoklassniki_1.svg?react";
 import Google from "./svg/google.svg?react";
 
-function App() {  
-  
-  const avaGirl = <img src={PersonGirl} alt="My SVG" />;
-  const avaBoy = <img src={PersonBoy} alt="My SVG" />;
-  const [person, setPerson] = useState(avaBoy);
-  const [auto, setAuto] = useState(true);
-  const [login, setLogin] = useState("");
+let toggle = 0;
 
-  function header() {
-    return (      
-      <header>
-        <div style={{
-          display: "flex", 
-          alignItems: "center"
-        }}
-        >
-        <div style={{ width: 250 }}>
-        <div id="logo">
-          <div id="logoText" style={{ 
-            color: "green",
-            letterSpacing: ".1rem"
-            }}>LEMENTALS</div>
-        </div>
+function header(setToggleState: any) {
+  return (      
+    <header>      
+      <div className="left_menu">
+      <div style={{ width: 250 ,
+                    float: "left"}}>
+      <div id="logo" title="Go to Elementals Home">
+        <div id="logoText">LEMENTALS</div>
       </div>
-        <div id="headerMenu">
-          <span className="menu">Episodes</span>
-          <span className="menu">About</span>
-          <span className="menu">More</span>
-        </div>
-        </div>
-        <div id="headerButton">
-          <button>SUBSCRIBE</button>
-          <div title="войти" style={
-            {width: 70, cursor: "pointer"}
-          } 
-          onMouseOver={() => setPerson(avaGirl)}
-          onMouseOut={() => setPerson(avaBoy)}
-          onClick={() => setAuto(authorization)}
-          >
-            {person}
-          </div>
-        </div>        
-      </header>
-    );
-  }
+    </div>
+      <div id="headerMenu">
+        <span className="menu">Episodes</span>
+        <span className="menu">About</span>
+        <span className="menu">More</span>
+      </div>
+      </div>
+      <div id="headerButton">
+        <button>SUBSCRIBE</button>
+        <button title="Log in to Elementals" onClick={() => (toggle == 0 ? toggle = 1 : 0, setToggleState(toggle))}>Log In</button>          
+      </div>        
+    </header>
+  );
+}
 
+function closeAutoWindow() {
+  return "";
+}
 
-  function authorization() {
-  
-    return (
-      <div className="authorizationBaze">
-        <div className="authorizationForm">        
-        <div style={
-        {
-          textAlign: "end",        
-          fontSize: "20px",
-          color: "aqua",
-          cursor: "pointer",
-          margin: "-50px -30px 0 0"
-        }
-      }
-      onClick={(e) => (e.preventDefault(),
-        setAuto(false)
-        // console.log(e.target)
-        )}
-      >X</div>
-      <div style={
-        {
-          display: "flex",
-          justifyContent: "space-between",
-          margin: "30px"
-        }
-      }>
-      <div>Авторизация</div>
-      <div className="social_networks">
-        <VK />
-        
-      </div>
-      </div>
-      <form style={{display: "flex",
-                  justifyContent: "space-around",
-                  alignItems: "center",
-                  flexDirection: "column"
-      }}      
-      >
-      <input className="autoInput" type="text" placeholder="Логин" onChange={(e: any) => console.log(e.target.value)} />
-      <input className="autoInput" type="text" placeholder="Пароль" 
-      onClick={() => console.log(login)}
-      />
-      <button className="autoInput" id="authorizationButton">Войти</button>
-      </form>
-      <div style={{display: "flex",
-                  justifyContent: "space-between",  
-                  margin: "20px 0 10px 15px"
-      }}>
-        <span className="menu">Регистрация</span><span className="menu">Забыли пароль?</span></div>
-      </div>
-      </div>
-    )
-  }  
- 
+function authorization() { 
+  const [close, setClose] = useState();  
 
   return (
-    <>
-      <>{auto}</>
-      <div>{header()}</div> 
+    <div className="authorizationBaze">
+      <div className="authorizationForm">        
+      <div className="autoClose">X</div>
+    <div className="autoTop">
+    <div>Авторизация</div>
+    <div>
+      <span className="social_networks"><VK /></span>
+      <span className="social_networks"><Facebook /></span>
+      <span className="social_networks"><Odnoklassniki /></span>
+      <span className="social_networks"><Google /></span>
+    </div>
+    </div>
+    <form className="autoForm">
+    <input className="autoInput" type="text" placeholder="Логин"/>
+    <input className="autoInput" type="text" placeholder="Пароль"/>
+    <button className="autoInput" id="authorizationButton">Войти</button>
+    </form>
+    <div className="autoBottom"><span className="menu">Регистрация</span><span className="menu">Забыли пароль?</span></div>
+    </div>
+    </div>
+  )
+}  
+
+function App() { 
+  const [toggleState, setToggleState] = useState(Number);
+  const [auto, setAuto] = useState(toggleState);
+  
+  return (
+    <>  
+      <>{toggleState == 0 ? console.log(0) : console.log(1)}</>  
+      
+      <>{auto}</> 
+      <div>{header(setToggleState)}</div> 
       
       
       
